d7ff5fcc91b223a9694705bf6011d37b72db1003
devil_1nc <diamondoriginal367@gmail.com>
2022-09-30 15:56:48 +0400

    Добавьте файлы проекта.

---
+++ MutriskovTiPEIS/FormChartOfAccounts.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+using System.IO;
+
+namespace MutriskovTiPEIS
+{
+    public partial class FormChartOfAccounts : Form
+    {
+        private SQLiteConnection sqlConn;
+        private SQLiteCommand sqlCmd;
+        private DataSet DS = new DataSet();
+        private DataTable DT = new DataTable();
+        private string sPath = Path.Combine(Application.StartupPath, "mydb.db");
+
+        public FormChartOfAccounts()
+        {
+            InitializeComponent();
+        }
+
+        private void FormChartOfAccounts_Load(object sender, EventArgs e)
+        {
+            string ConnectionString = @"Data Source=" + sPath + ";New=False;Version=3";
+            string selectCommand = "Select * from ChartOfAccounts";
+            SelectTable(ConnectionString, selectCommand);
+        }
+
+        private void SelectTable(string conString, string selectCmd)
+        {
+            SQLiteConnection connect = new SQLiteConnection(conString);
+            connect.Open();
+            SQLiteDataAdapter dataAdapter = new SQLiteDataAdapter(selectCmd, connect);
+            DataSet ds = new DataSet();
+            dataAdapter.Fill(ds);
+            dataGridView.DataSource = ds;
+            dataGridView.DataMember = ds.Tables[0].ToString();
+            connect.Close();
+        }
+    }
+}
